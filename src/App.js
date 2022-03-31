dd109f82e50ad3ff9cf550c79b8fe142a9504c21
Trung Nguyen <nghtrung1001@gmail.com>
2022-03-31 13:03:38 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import LogoCarousel from './components/LogoCarousel';
 import Navbar from './components/Navbar';
 import Body from './components/Body'
 function App() {
-  return (
-    <div className="px-3 py-6">
-       <Navbar /> 
-       <Header />
-       <LogoCarousel />
-       <Body />
-    </div>
+return (
+  <>
+  <Navbar /> 
+  <Header />
+  <LogoCarousel />
+  <Body />
+  </>
   );
 }
 
